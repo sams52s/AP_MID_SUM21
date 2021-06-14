f3eb5d69a2853746cdd1722c48d074cd9f720d0b
Sams52 <sams52tas@gmail.com>
2021-06-14 17:13:12 +0600

    ASP MVC .net Lab Task. Student CRUD task

---
+++ LabTask1sln/LabTask1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTask1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+           
+
+            return View();
+        }
+    }
+}
