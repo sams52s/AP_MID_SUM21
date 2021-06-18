781e41b9a8eeed0f396ec679dd2176391448fa50
Sams52 <sams52tas@gmail.com>
2021-06-19 01:33:36 +0600

    Database works

---
+++ DBhandlingsln/DBhandling/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBhandling.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult reginfo()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult StudentInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ DBhandlingsln/DBhandling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBhandling.MvcApplication" Language="C#" %>
