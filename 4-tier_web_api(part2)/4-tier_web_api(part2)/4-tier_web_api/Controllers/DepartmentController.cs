306a1b3ed4a3f91656646408c1ff126214471c08
Sams52 <sams52tas@gmail.com>
2021-07-28 02:59:41 +0600

    4-tier_web_api part2 after adding given app

---
+++ 4-tier_web_api(part2)/4-tier_web_api(part2)/4-tier_web_api/Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using BEL;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _4_tier_web_api.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        [Route("api/Department/Names")]
+        [HttpGet]
+        public List<string> GetNames()
+        {
+            return DepartmentService.GetDepartmentNames();
+        }
+        [Route("api/Department/GetAll")]
+        [HttpGet]
+        public List<DepartmentModel>GetAllDepartments()
+        {
+            return DepartmentService.GerDepartmenrs();
+        }
+        [Route("api/Department/Add")]
+        [HttpPost]
+        public void Add(DepartmentModel dept)
+        {
+            DepartmentService.AddDepartment(dept);
+        }
+    }
+}
---
+++ 4-tier_web_api(part2)/4-tier_web_api(part2)/BLL/DepartmentService.cs
@@ -0,0 +1,43 @@
+﻿using BEL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class DepartmentService
+    {
+        public static List<string> GetDepartmentNames()
+        {
+            return DepartmentRepo.GetDepartmentNames();
+            
+        }
+        public static List<DepartmentModel> GerDepartmenrs()
+        {
+            var departments = DepartmentRepo.GetDepartments();
+            List<DepartmentModel> data = new List<DepartmentModel>();
+            foreach(var d in departments) {
+                var dm = new DepartmentModel()
+                {
+                    Id = d.Id,
+                    Name = d.Name
+                };
+                data.Add(dm);
+            }
+            return data;
+        }
+        public static void AddDepartment(DepartmentModel dept)
+        {
+            var d = new Department()
+            {
+                Id = dept.Id,
+                Name = dept.Name
+            };
+            DepartmentRepo.AddDepartment(d);
+        }
+
+    }
+}
